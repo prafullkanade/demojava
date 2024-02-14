e5d39619a770cfc3365fd8797cd1c79d9be18c3f
prafullkanade <prafaws@gmail.com>
2024-02-14 10:38:08 +0000

    code changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // prints "hello world" in terminal window
         System.out.println("Hello, World");
+	System.out.println("Change from git scm");
     }
 }
